1e93a4495db75acaeaa3ffe599b1d829a89017fe
joshwrn <joshnwarren@gmail.com>
2021-06-24 23:50:15 -0700

    starting context menu

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import boardPhoto from '../assets/img/picture.jpg';
 import '../styles/photo.css';
+import '../styles/menu.css';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -8,15 +9,18 @@ import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  });
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -24,25 +28,29 @@ const firestore = firebase.firestore();
 const Mouse = () => {
   const [tagLocation, setTagLocation] = useState({ x: 0, y: 0 });
   const [itemsArray, setItemsArray] = useState([]);
+  const [contextMenu, setContextMenu] = useState({ display: 'none' });
   const { x, y } = tagLocation;
   const temp = [];
-  let arrl = '';
 
-  const verifyItem = async (itemName) => {
-    await firestore
+  const verifyItem = async (item) => {
+    const results = await firestore
       .collection('levels')
       .doc('celebration')
       .collection('items')
+      .doc(item.text)
       .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          if (doc.data().text === itemName) {
-            console.log('verify true');
-            arrl = 'test';
-            return true;
-          }
-        });
+      .then((doc) => {
+        if (
+          doc.data().coordX >= item.coordX - 50 &&
+          doc.data().coordX <= item.coordX + 50 &&
+          doc.data().coordY >= item.coordY - 50 &&
+          doc.data().coordY <= item.coordY + 50
+        ) {
+          console.log('Document data:', doc.data());
+          return true;
+        }
       });
+    return results;
   };
 
   // get item list once on load
@@ -71,15 +79,14 @@ const Mouse = () => {
     }
   });
 
-  async function testLocation(e) {
+  async function testLocation(e, itemName) {
     e.preventDefault();
-    if (findItem !== undefined) {
+    if (findItem !== undefined && findItem.chosen !== true) {
       console.log(findItem.text);
       try {
-        const verifyTest = await verifyItem(findItem.text);
+        const verifyTest = await verifyItem(findItem);
         console.log(verifyTest);
-        console.log(arrl);
-        if (arrl === 'test') {
+        if (verifyTest === true) {
           console.log('please');
           const itemIndex = itemsArray.findIndex(
             (item) => item.text === findItem.text
@@ -109,15 +116,56 @@ const Mouse = () => {
     }));
   };
 
-  const testFunction = () => {
-    console.log(itemsArray);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setContextMenu((state) => ({
+      ...state,
+      display: 'block',
+    }));
+  };
+
+  const handleSelection = (e) => {
+    e.preventDefault();
+    const itemName = e.target.getAttribute('data-text');
+    testLocation(e, itemName);
+    setContextMenu((state) => ({
+      ...state,
+      display: 'none',
+    }));
   };
 
   return (
     <div>
+      <div
+        id="menu"
+        style={{
+          top: y,
+          left: x,
+          display: contextMenu.display,
+        }}
+      >
+        {itemsArray.map((item) => {
+          return (
+            <p
+              style={
+                item.chosen === true
+                  ? {
+                      textDecoration: 'line-through',
+                    }
+                  : null
+              }
+              key={item.text}
+              data-text={item.text}
+              onClick={item.chosen !== true ? handleSelection : null}
+            >
+              {item.text}
+            </p>
+          );
+        })}
+      </div>
       <img
-        onMouseMove={onMouseMove}
-        onClick={testLocation}
+        onMouseMove={contextMenu.display === 'none' ? onMouseMove : null}
+        onClick={handleClick}
         className="picture"
         src={boardPhoto}
         alt=""
