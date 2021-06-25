3c0dbb4d4e32f0c2c3dfd2604f909e1342eeb1d4
joshwrn <joshnwarren@gmail.com>
2021-06-24 21:05:25 -0700

    trying to fetch single doc

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,53 +1,5 @@
-import React from 'react';
-
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-firebase.initializeApp({
-  apiKey: 'AIzaSyADxCQy9_A6MbGEvROV64IHMifVZaec5hQ',
-  authDomain: 'waldo-37e04.firebaseapp.com',
-  projectId: 'waldo-37e04',
-  storageBucket: 'waldo-37e04.appspot.com',
-  messagingSenderId: '856550100154',
-  appId: '1:856550100154:web:13b82c490bac63e55fc683',
-});
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-function ChatRoom() {
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-  const [messages] = useCollectionData(query, { idField: 'id' });
-  return (
-    <>
-      <main>
-        {messages &&
-          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-      </main>
-    </>
-  );
-}
-
-function ChatMessage(props) {
-  const { text } = props.message;
-  return (
-    <>
-      <p>{text}</p>
-    </>
-  );
-}
-
 const Board = () => {
-  return (
-    <div>
-      <ChatRoom />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Board;
--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -1,9 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import boardPhoto from '../assets/img/picture.jpg';
 import '../styles/photo.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 const Mouse = () => {
   const [tagLocation, setTagLocation] = useState({ x: 0, y: 0 });
+  const [itemsArray, setItemsArray] = useState([]);
+  const { x, y } = tagLocation;
+  const temp = [];
+  let arrl = '';
+
+  const verifyItem = async (itemName) => {
+    await firestore
+      .collection('levels')
+      .doc('celebration')
+      .collection('items')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          if (doc.data().text === itemName) {
+            console.log('verify true');
+            arrl = 'test';
+            return true;
+          }
+        });
+      });
+  };
+
+  // get item list once on load
+  useEffect(() => {
+    firestore
+      .collection('levels')
+      .doc('celebration')
+      .collection('items')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          temp.push(doc.data());
+        });
+        setItemsArray(temp);
+      });
+  }, []);
+
+  const findItem = itemsArray.find((item) => {
+    if (
+      x >= item.coordX - 50 &&
+      x <= item.coordX + 50 &&
+      y >= item.coordY - 50 &&
+      y <= item.coordY + 50
+    ) {
+      return item.text;
+    }
+  });
+
+  async function testLocation(e) {
+    e.preventDefault();
+    if (findItem !== undefined) {
+      console.log(findItem.text);
+      try {
+        const verifyTest = await verifyItem(findItem.text);
+        console.log(verifyTest);
+        console.log(arrl);
+        if (arrl === 'test') {
+          console.log('please');
+          const itemIndex = itemsArray.findIndex(
+            (item) => item.text === findItem.text
+          );
+          setItemsArray(
+            (prev) => [...prev],
+            {
+              [itemsArray[itemIndex]]: (itemsArray[itemIndex].chosen = true),
+            },
+            console.log(itemsArray)
+          );
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log('wrong');
+      console.log(itemsArray);
+    }
+  }
 
   const onMouseMove = (e) => {
     setTagLocation((state) => ({
@@ -13,11 +109,19 @@ const Mouse = () => {
     }));
   };
 
-  const { x, y } = tagLocation;
+  const testFunction = () => {
+    console.log(itemsArray);
+  };
 
   return (
-    <div onMouseMove={onMouseMove}>
-      <img className="picture" src={boardPhoto} alt="" />
+    <div>
+      <img
+        onMouseMove={onMouseMove}
+        onClick={testLocation}
+        className="picture"
+        src={boardPhoto}
+        alt=""
+      />
       <h1>
         Mouse coordinates: {x} {y}
       </h1>
