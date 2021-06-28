a01ad60c460a3d958e2444664357560818dd05ad
joshwrn <joshnwarren@gmail.com>
2021-06-27 20:18:59 -0700

    firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 const db = () => {
   if (!firebase.apps.length) {
     return firebase.initializeApp({
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      apiKey: 'AIzaSyADxCQy9_A6MbGEvROV64IHMifVZaec5hQ',
+      authDomain: 'waldo-37e04.firebaseapp.com',
+      projectId: 'waldo-37e04',
+      storageBucket: 'waldo-37e04.appspot.com',
+      messagingSenderId: '856550100154',
+      appId: '856550100154:web:13b82c490bac63e55fc683',
     });
   } else {
     return firebase.app(); // if already initialized, use that one
